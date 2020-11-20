0110aa590c4e39f810a01d183858e562d1d681b8
Le Van Phu <phule.ou@gmail.com>
2020-11-20 22:08:04 +0700

    Add new class Search

---
+++ testcases/com/wordpress/posts/Post_02_Search.java
@@ -0,0 +1,19 @@
+package com.wordpress.posts;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Post_02_Search {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
