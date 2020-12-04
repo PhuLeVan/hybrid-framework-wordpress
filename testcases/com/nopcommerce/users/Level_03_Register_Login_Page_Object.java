e9b4a4320f21300193edba678c203b9b6abb1d08
Le Van Phu <phule.ou@gmail.com>
2020-12-04 23:35:33 +0700

    Create new Nopcommerce project

---
+++ testcases/com/nopcommerce/users/Level_03_Register_Login_Page_Object.java
@@ -0,0 +1,20 @@
+package com.nopcommerce.users;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Level_03_Register_Login_Page_Object {
+	@BeforeClass
+	public void beforeClass() {
+	}
+	
+	@Test
+	public void f() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
